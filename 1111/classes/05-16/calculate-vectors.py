2e802270e9c15236e017ffcef4a83aab030a2a8c
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2019-05-16 08:10:11 -0400

    Minor change

--- 1111/classes/05-16/calculate-vectors.py
+++ 1111/classes/05-16/calculate-vectors.py
@@ -3,12 +3,12 @@
 import random
 import math
 
-print ""
-print "##########"
-print "Author: Dr. Nathan Harrison"
-print "Instructions: Do it."
-print "Comments: All angles are in degrees and defined counter-clockwise with respect to the x-axis."
-print "##########"
+print ("")
+print ("##########")
+print ("Author: Dr. Nathan Harrison")
+print ("Instructions: Do it.")
+print ("Comments: All angles are in degrees and defined counter-clockwise with respect to the x-axis.")
+print ("##########")
 
 names = ["Vx", "Vy", "magnitude", "theta"]
 
@@ -30,29 +30,29 @@ for j in range(1, 16):
   indices.remove(ri1)
   indices.remove(ri2)
   
-  print ""
-  print "###", j, "###"
-  print "First draw a picture of the vector!"
-  print "Given ", names[ri1], " = ", vals[ri1], " and ", names[ri2], " = ", vals[ri2]
+  print ("")
+  print ("###", j, "###")
+  print ("First draw a picture of the vector!")
+  print ("Given ", names[ri1], " = ", vals[ri1], " and ", names[ri2], " = ", vals[ri2])
   
   correct = False
   while correct == False:
     ans = float(input("What is " + names[indices[0]] + "? "))
     if (ans > vals[indices[0]]-0.025 and ans < vals[indices[0]]+0.025) or ans == 1234:
-      print "good"
+      print ("good")
       correct = True
     else:
-      print "nope"
+      print ("nope")
   
   correct = False
   while correct == False:
     ans = float(input("What is " + names[indices[1]] + "? "))
     if (ans > vals[indices[1]]-0.025 and ans < vals[indices[1]]+0.025) or ans == 1234:
-      print "good"
+      print ("good")
       correct = True
     else:
-      print "nope"
+      print ("nope")
 
-print ""
-print "Woo-hoo"
+print ("")
+print ("Woo-hoo")
 
